08468bfc0291bb5ce700e5ee175931ef923322d3
Matthew <matthewpw36@gmail.com>
2023-09-25 09:15:51 -0400

    started server outline

---
+++ HW2/section4/MainServer.cpp
@@ -0,0 +1,24 @@
+void publishData(){
+    //wait for movement update
+
+    //move entity
+
+    //check for collisions
+
+    //send update of moved entities
+}
+
+void platformsThread(){
+    //move platforms
+
+    //send updates to publish socket
+}
+
+
+int main(){
+    //wait for player movement
+
+    //send updates to publish socket
+
+    
+}
